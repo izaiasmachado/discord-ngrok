4c44cd448d41062977344cf45e9025d7dfe4dea9
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-08-24 00:05:45 -0300

    feat: enable choose protocol

--- src/utils/ngrok.js
+++ src/utils/ngrok.js
@@ -1,29 +1,29 @@
-const ngrok = require('ngrok')
-const axios = require('axios')
+const ngrok = require("ngrok");
+const axios = require("axios");
 
-const { NGROK_PORT, NGROK_AUTH, NGROK_AUTH_TOKEN, NGROK_REGION } = process.env
+const { NGROK_PORT, NGROK_AUTH, NGROK_AUTH_TOKEN, NGROK_REGION } = process.env;
 
-async function connect () {
-    await ngrok.connect({
-        proto: 'tcp',
-        addr: Number(NGROK_PORT),
-        auth: NGROK_AUTH,
-        authtoken: NGROK_AUTH_TOKEN,
-        region: NGROK_REGION
-    })
-    
-    return await getIp()
+async function connect() {
+  await ngrok.connect({
+    proto: NGROK_PROTOCOL,
+    addr: Number(NGROK_PORT),
+    auth: NGROK_AUTH,
+    authtoken: NGROK_AUTH_TOKEN,
+    region: NGROK_REGION,
+  });
+
+  return await getIp();
 }
 
-function kill () {
-    return ngrok.kill()
+function kill() {
+  return ngrok.kill();
 }
 
-async function getIp () {
-    const apiUrl = ngrok.getUrl()
-    const response = await axios.get(`${apiUrl}/api/tunnels`)
-    const tunnelIp = response.data.tunnels[0].public_url 
-    return tunnelIp.slice(6)
+async function getIp() {
+  const apiUrl = ngrok.getUrl();
+  const response = await axios.get(`${apiUrl}/api/tunnels`);
+  const tunnelIp = response.data.tunnels[0].public_url;
+  return tunnelIp.slice(6);
 }
 
-module.exports = { connect, kill, getIp }
+module.exports = { connect, kill, getIp };
